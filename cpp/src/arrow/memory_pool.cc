8b1b13e2d5744c0aed35cb7fae5a153b1443b93a
Sadman Sakib <sadmankiba@cs.wisc.edu>
2024-03-29 19:43:17 +0000

    Use prev version

--- cpp/src/arrow/memory_pool.cc
+++ cpp/src/arrow/memory_pool.cc
@@ -277,45 +277,41 @@ class PreallocAllocator {
  public:
   // Allocate memory according to the alignment requirements for Arrow
   // (as of May 2016 64 bytes)
-  static Status AllocateAligned(int64_t size, int64_t alignment, uint8_t** out) {    
+  static Status AllocateAligned(int64_t size, uint8_t** out) {
     if (size == 0) {
-      *out = memory_pool::internal::kZeroSizeArea;
+      *out = zero_size_area;
       return Status::OK();
     }
-    const int result =
-        posix_memalign(reinterpret_cast<void**>(out), static_cast<size_t>(alignment),
-                       static_cast<size_t>(size));
+    const int result = posix_memalign(reinterpret_cast<void**>(out), kAlignment,
+                                      static_cast<size_t>(size));
     if (result == ENOMEM) {
       return Status::OutOfMemory("malloc of size ", size, " failed");
     }
 
     if (result == EINVAL) {
-      return Status::Invalid("invalid alignment parameter: ",
-                             static_cast<size_t>(alignment));
+      return Status::Invalid("invalid alignment parameter: ", kAlignment);
     }
     std::cout << "Allocated from PreallocAllocator: size = " << size << ", alignment = " << alignment << std::endl;
 
     return Status::OK();
   }
 
-  static Status ReallocateAligned(int64_t old_size, int64_t new_size, int64_t alignment,
-                                  uint8_t** ptr) {
+  static Status ReallocateAligned(int64_t old_size, int64_t new_size, uint8_t** ptr) {
     uint8_t* previous_ptr = *ptr;
-    if (previous_ptr == memory_pool::internal::kZeroSizeArea) {
+    if (previous_ptr == zero_size_area) {
       DCHECK_EQ(old_size, 0);
-      return AllocateAligned(new_size, alignment, ptr);
+      return AllocateAligned(new_size, ptr);
     }
     if (new_size == 0) {
-      DeallocateAligned(previous_ptr, old_size, alignment);
-      *ptr = memory_pool::internal::kZeroSizeArea;
+      DeallocateAligned(previous_ptr, old_size);
+      *ptr = zero_size_area;
       return Status::OK();
     }
     // Note: We cannot use realloc() here as it doesn't guarantee alignment.
 
     // Allocate new chunk
-
     uint8_t* out = nullptr;
-    RETURN_NOT_OK(AllocateAligned(new_size, alignment, &out));
+    RETURN_NOT_OK(AllocateAligned(new_size, &out));
     DCHECK(out);
     // Copy contents and release old memory chunk
     memcpy(out, *ptr, static_cast<size_t>(std::min(new_size, old_size)));
@@ -328,8 +324,8 @@ class PreallocAllocator {
     return Status::OK();
   }
 
-  static void DeallocateAligned(uint8_t* ptr, int64_t size, int64_t /*alignment*/) {
-    if (ptr == memory_pool::internal::kZeroSizeArea) {
+  static void DeallocateAligned(uint8_t* ptr, int64_t size) {
+    if (ptr == zero_size_area) {
       DCHECK_EQ(size, 0);
     } else {
       free(ptr);
